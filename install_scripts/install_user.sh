94b2809bf25c86aa5ad4b77bbac9b0e0c2ccbd5f
fmanier <contact@webtotem.fr>
2021-03-26 09:15:25 +0100

    fix(user) : test if group exists to create it if not

--- install_scripts/install_user.sh
+++ install_scripts/install_user.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -e
 
-user=$(grep ":${HOST_UID}" < /etc/group | cut -d":" -f1)
+user=$(grep "x:${HOST_UID}" < /etc/passwd | cut -d":" -f1)
+group=$(grep "x:${HOST_GID}" < /etc/group | cut -d":" -f1)
+
+if [ ! "$group" ]; then
+  group="tux"
+  groupadd -g "${HOST_GID}" $group
+fi
 
 if [ ! "$user" ]; then
   user="tux"
-
-  groupadd -g "${HOST_GID}" $user
-
   useradd -m  -u "${HOST_UID}" -g "${HOST_GID}" -s /bin/bash $user
-
 fi
 
 apt install -y sudo
-# ensures that the user can logging in with bash shell
-usermod -aG sudo -s /bin/bash $user
+# ensures that the user can logging in with bash shell and is in the group defined above
+usermod -aG sudo,$group -s /bin/bash $user
 
 # set default passwd to tux user
 passwd="azertyuiop"
